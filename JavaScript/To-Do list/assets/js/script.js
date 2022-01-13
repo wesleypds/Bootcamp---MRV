ffb4fb838527f8f189ab875c71d55a648d9a552c
wesleypds <wlypereiraa@gmail.com>
2022-01-13 18:09:43 -0300

    Att To Do List

--- JavaScript/To-Do list/assets/js/script.js
+++ JavaScript/To-Do list/assets/js/script.js
@@ -4,15 +4,22 @@ const taskList = document.getElementById('tasks');
 form.onsubmit = function (e) {
 	e.preventDefault();
 	const inputField = document.getElementById('task-input');
-	addTask(inputField.value);
+	if (inputField.value === '') {
+		alert('Insira uma tarefa!');
+	}else{
+		addTask(inputField.value);
+	}
 	form.reset();
 };
 
 function addTask(description) {
+	const taskCancel = document.createElement('span');
 	const taskContainer = document.createElement('div');
 	const newTask = document.createElement('input');
+	const parent = newTask.parentNode;
 	const taskLabel = document.createElement('label');
 	const taskDescriptionNode = document.createTextNode(description);
+	const taskDescriptionCancel = document.createTextNode('highlight_off');
 
 	newTask.setAttribute('type', 'checkbox');
 	newTask.setAttribute('name', description);
@@ -20,14 +27,23 @@ function addTask(description) {
 
 	taskLabel.setAttribute('for', description);
 	taskLabel.appendChild(taskDescriptionNode);
+	taskLabel.classList.add('txt-style')
     taskLabel.addEventListener('click', a = () => {
         taskLabel.classList.toggle('lineThrough');
-        return;
+        return false;
     });
 
 	taskContainer.classList.add('task-item');
 	taskContainer.appendChild(newTask);
 	taskContainer.appendChild(taskLabel);
+	taskContainer.appendChild(taskCancel);
+
+	taskCancel.classList.add('material-icons');
+	taskCancel.appendChild(taskDescriptionCancel);
+	taskCancel.addEventListener('click', a = () => {
+			parent.removeChild(newTask);
+		return false;
+	});
 
 	taskList.appendChild(taskContainer);
 }
