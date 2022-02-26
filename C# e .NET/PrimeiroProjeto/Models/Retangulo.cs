0a6f6e102088246b08a5f945056a14497b9fbc2a
wesleypds <wlypereiraa@gmail.com>
2022-02-26 03:20:32 -0300

    att C# e .NET

---
+++ C# e .NET/PrimeiroProjeto/Models/Retangulo.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace PrimeiroProjeto.Models
+{
+    public class Retangulo
+    {
+        private double Comprimento;
+        private double Largura;
+        private bool Valido;
+        public void DefMed(double comp, double larg)
+        {
+            if (comp > 0 && larg > 0)
+            {
+                this.Comprimento = comp;
+                this.Largura = larg;
+                this.Valido = true;
+            }else
+            {
+                Console.WriteLine($"Valores inválidos!! Segue os valores: {comp}, {larg}");
+            }
+        }
+        public double ObterArea()
+        {
+            if (Valido)
+            {
+                return Comprimento * Largura;
+            }
+            else
+            {
+                Console.WriteLine("Preencha com valores válidos.");
+                return 0;
+            }
+        }
+    }
+}
--- C# e .NET/PrimeiroProjeto/Program.cs
+++ C# e .NET/PrimeiroProjeto/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using PrimeiroProjeto.Models;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            Retangulo med = new Retangulo();
+
+            p1.Nome = "Wesley Pereira";
+            p1.Idade = 25;
+            p1.Apresentar();
+
+            med.DefMed(0,10);
+            Console.WriteLine($"A área do rentangulo é {med.ObterArea()}");
+        }
+    }
+}
